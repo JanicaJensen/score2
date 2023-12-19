8e1761cc031afd0d313f710b3f20c5f493831100
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-12-19 08:55:46 -0700

    deleted player model

--- models/player.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const playerSchema = new Schema({
-  name: String,
-  scores: [Number],
-});
-
-module.exports = mongoose.model("Player", playerSchema);
