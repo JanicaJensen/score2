107b8990ea3f77b743a6e6509f39085191189b16
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-12-19 07:06:59 -0700

    added env port

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get("/", async (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("serving on port 3000");
 });
